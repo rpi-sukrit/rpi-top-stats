13bb7115a15972c6c665e52c8381871b82b29aa7
RPI Sukrit <>
2017-10-28 14:11:04 -0400

    TOP STAT

--- pushTopStat.sh
+++ pushTopStat.sh
@@ -1,6 +1,21 @@
+#CONSTANTS
+date=$(date)
+
+#TOP
 (top -n 1 -b | head -n 20) > top.txt
-curl http://192.168.0.100/admin/api.php  > piholeAPI.json
-git add ./top.txt
-git add ./piholeAPI.json
+
+#Pihole API
+curl http://pi.hole/admin/api.php  > piholeAPI.json
+
+#RPI STATS
+temp=$(vcgencmd measure_temp)
+tempOutput="{\"date\":\"$date\",\"temp\":\"$temp\"}"
+volt=$(vcgencmd measure_volts core)
+voltOutput="{\"date\":\"$date\",\"volt\":\"$volt\"}"
+echo "$tempOutput">temp.json
+echo "$voltOutput">corevolts.json
+
+#GIT 
+git add .
 git commit -m "TOP STAT" --author "RPI Sukrit <>"
 git push -u origin master
